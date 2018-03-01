2fa00a9560c52fa7514ca8db2ea117b741d42c2e
YureAlbuquerque <yurealbuquerque@lme.ufc.br>
2018-03-01 15:05:12 -0300

    Adição de Cronometro.ino para testes

---
+++ Codigos_testes/Cronometro/Cronometro.ino
@@ -0,0 +1,97 @@
+const int Sensor1 = 8;
+const int Sensor2 = 9;
+const int Sensor3 = 10;
+const int Sensor4 = 11;
+const int Sensor5 = 12;
+
+unsigned long t0; // tempo inicial
+unsigned long t1; // tempo final no sensor2
+unsigned long t2; // tempo final no sensor3
+unsigned long t3; // tempo final no sensor4
+unsigned long t4; // tempo final no sensor5
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(Sensor1, INPUT);
+  pinMode(Sensor2, INPUT);
+  pinMode(Sensor3, INPUT);
+  pinMode(Sensor4, INPUT);
+  pinMode(Sensor5, INPUT);
+}
+
+void loop()
+{
+  //Serial.print("Iniciando em 2s...");
+  delay(2000); // aguarda dois segundos
+  
+  // Sensor1 -------------------------------------------
+  Serial.println("Aguardando a resposta do Sensor1...");
+  while(digitalRead(Sensor1) == LOW)
+  {
+    ; // Aguarda o primeiro sensor.
+  }
+  
+  t0 = millis(); // captura o tempo corrente em t0
+  //Serial.print("t0 (ms) = "); Serial.println(t0);
+  
+  // Sensor2 -------------------------------------------
+  //Serial.println("Aguardando a resposta do Sensor2...");
+  while(digitalRead(Sensor2) == LOW)
+  {
+    ; // Aguarda o segundo objeto.
+  }
+  
+  t1 = millis(); // captura o tempo corrente em t1
+  //Serial.print("t1 (ms) = "); Serial.println(t1);
+  
+  //Sensor3---------------------------------------------
+    //Serial.println("Aguardando a resposta do Sensor3...");
+  while(digitalRead(Sensor3) == LOW)
+  {
+    ; // Aguarda o segundo objeto.
+  }
+  
+  t2 = millis(); // captura o tempo corrente em t2
+  //Serial.print("t2 (ms) = "); Serial.println(t2);
+  
+  //Sensor4---------------------------------------------
+    //Serial.println("Aguardando a resposta do Sensor4...");
+  while(digitalRead(Sensor4) == LOW)
+  {
+    ; // Aguarda o segundo objeto.
+  }
+  
+  t3 = millis(); // captura o tempo corrente em t3
+  //Serial.print("t3 (ms) = "); Serial.println(t3);
+  
+  
+  //Sensor5---------------------------------------------
+   // Serial.println("Aguardando a resposta do Sensor5...");
+  while(digitalRead(Sensor5) == LOW)
+  {
+    ; // Aguarda o segundo objeto.
+  }
+  
+  t4 = millis(); // captura o tempo corrente em t4
+  //Serial.print("t4 (ms) = "); Serial.println(t4);
+  
+  
+  
+  
+  
+  // Calculo -------------------------------------------
+  //Serial.print("Tempo decorrido 1_2 (ms): ");
+  Serial.println(t1-t0);
+  //Serial.print("Tempo decorrido 1_3 (ms): ");
+  Serial.println(t2-t0);
+  //Serial.print("Tempo decorrido 1_4 (ms): ");
+  Serial.println(t3-t0);
+  //Serial.print("Tempo decorrido 1_5 (ms): ");
+  Serial.println(t4-t0);  
+  
+ 
+  Serial.println("Experimento finalizado....");
+  //Serial.println("Reset o Arduino...");
+ // while (1); // loop infinito. reset o arduino para um novo experimento.
+} 
