69e4b2552cee382d3f38a45f319315218a3c8e54
Yure <yurevsa@hotmail.com>
2018-03-15 16:39:05 -0300

    Remoção do buzzer no Cronometro

--- Codigos_testes/Cronometro/Cronometro.ino
+++ Codigos_testes/Cronometro/Cronometro.ino
@@ -3,6 +3,7 @@ const int Sensor2 = A1;
 const int Sensor3 = A2;
 const int Sensor4 = A3;
 const int Sensor5 = A4;
+//const int pinTone = 8;
 
 unsigned long t0; // tempo inicial
 unsigned long t1; // tempo final no sensor2
@@ -18,6 +19,7 @@ void setup()
   pinMode(Sensor3, INPUT);
   pinMode(Sensor4, INPUT);
   pinMode(Sensor5, INPUT);
+  //pinMode(pinTone, OUTPUT);
 }
 
 void loop()
@@ -33,15 +35,15 @@ void loop()
   }
   t0 = millis(); // captura o tempo corrente em t0
   Serial.print("t0 (ms) = "); Serial.println(t0);
-
+  //tone(pinTone, 800,50);
   // Sensor2 -------------------------------------------
   Serial.println("Aguardando a resposta do Sensor2...");
   while (digitalRead(Sensor2) == HIGH);
   while (digitalRead(Sensor2) == LOW);
-
+  
   t1 = millis(); // captura o tempo corrente em t1
   Serial.print("t1 (ms) = "); Serial.println(t1);
-
+  //tone(pinTone, 800,50);
   //Sensor3---------------------------------------------
   Serial.println("Aguardando a resposta do Sensor3...");
   while (digitalRead(Sensor3) == HIGH);
@@ -49,7 +51,7 @@ void loop()
 
   t2 = millis(); // captura o tempo corrente em t2
   Serial.print("t2 (ms) = "); Serial.println(t2);
-
+  //tone(pinTone, 800,50);
   //Sensor4---------------------------------------------
   Serial.println("Aguardando a resposta do Sensor4...");
   while (digitalRead(Sensor4) == HIGH);
@@ -57,8 +59,7 @@ void loop()
 
   t3 = millis(); // captura o tempo corrente em t3
   Serial.print("t3 (ms) = "); Serial.println(t3);
-
-
+  //tone(pinTone, 800,50);
   //Sensor5---------------------------------------------
   Serial.println("Aguardando a resposta do Sensor5...");
   while (digitalRead(Sensor5) == HIGH);
@@ -66,7 +67,7 @@ void loop()
 
   t4 = millis(); // captura o tempo corrente em t4
   Serial.print("t4 (ms) = "); Serial.println(t4);
-
+  //tone(pinTone, 800,50);
 
 
 
@@ -81,7 +82,8 @@ void loop()
   //Serial.print("Tempo decorrido 1_5 (ms): ");
   Serial.println(t4 - t0);
 
-
+  delay(1000);
+  //notone(pinTone);
   Serial.println("Experimento finalizado....");
   Serial.println("Reset o Arduino...");
   while (1); // loop infinito. reset o arduino para um novo experimento.
