5be84e7526462249cd18e5f338e5858f183ccd0c
YureAlbuquerque <yurealbuquerque@lme.ufc.br>
2018-03-26 20:52:10 -0300

    Novo teste para celula de carga

--- Codigos_testes/Celula_de_Carga_002/Celula_de_Carga_002.ino
+++ Codigos_testes/Celula_de_Carga_002/Celula_de_Carga_002.ino
@@ -1,20 +1,21 @@
 #include "HX711.h"
 
-HX711 celula;
+HX711 cell;
 
 void setup() {
   Serial.begin(115200);
   // parameter "gain" is ommited; the default value 128 is used by the library
   // HX711.DOUT  - pin #A1
   // HX711.PD_SCK - pin #A0
-  celula.begin(A1, A0, 128);//.., .., Gain
-  celula.set_offset(4293966000);
-  celula.set_scale(10000.0);
+  cell.begin(A1, A0, 64);//.., .., Gain
+  //  cell.set_offset(4018518);
+  cell.set_scale(10000.0);
 }
 
 void loop() {
-//  Serial.println(celula.read(), 1);
-//Serial.println(celula.get_value(), 1);
-Serial.println(celula.get_units());
-  delay(10);
+  //  Serial.println(cell.read(), 1);
+  //Serial.println(cell.get_value(), 1);
+  //cell.power_up();
+  Serial.println(cell.get_units());
+  delay(1);
 }
---
+++ Codigos_testes/Celula_de_Carga_003/Celula_de_Carga_003.ino
@@ -0,0 +1,42 @@
+#include "HX711.h"
+
+HX711 cell;
+double tare=0;
+
+void setup() {
+  Serial.begin(115200);
+  // parameter "gain" is ommited; the default value 128 is used by the library
+  // HX711.DOUT  - pin #A1
+  // HX711.PD_SCK - pin #A0
+  cell.begin(A1, A0, 64);//.., .., Gain
+  //  cell.set_offset(4018518);
+  cell.set_scale(10000.0);
+  //  tempo para calibração
+  Serial.println("Estabilizando");
+  long time1 = millis();
+  while (millis() - time1 < 10000);
+  Serial.println("Calibrando");
+  time1 = millis();
+  int count;
+  while (millis() - time1 < 10000) {
+    tare = tare + cell.get_units();
+    count++;
+    delay(10);
+    Serial.print(".");
+  }
+  Serial.println();
+  tare=tare/count;
+  Serial.println("Tara =" + String(tare));
+}
+
+void loop() {
+  //  Serial.println(cell.read(), 1);
+  //Serial.println(cell.get_value(), 1);
+  //cell.power_up();
+  double value = cell.get_units();
+  Serial.print("valor lido: ");
+  Serial.print(value);
+  Serial.print("     valor corrigido: ");
+  Serial.println(value - tare);
+  delay(10);
+}
