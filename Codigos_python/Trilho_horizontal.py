fdea9b893e6b3e2e64ea7b5a5cb7628fa7cad96f
YureAlbuquerque <yurealbuquerque@lme.ufc.br>
2018-03-28 17:46:04 -0300

    Novo código para trilho simples

--- Codigos_python/Trilho_horizontal.py
+++ Codigos_python/Trilho_horizontal.py
@@ -2,10 +2,13 @@ import matplotlib.pylab as plt
 import numpy as np
 import serial
 
-b_rate = 115200 #baud rate
-porta = 'COM22'  #nome da porta criada para o arduino
+#baud rate
+b_rate = 115200
+#nome da porta criada para o arduino
+porta = 'COM22'
+
 ser = serial.Serial(porta,b_rate)
-print 'Aguardando o termino do experimento...'
+print u'Aguardando o término do experimento...'
 
 temp_1 = ser.readline()
 		
@@ -14,13 +17,15 @@ temp_2 = ser.readline()
 temp_3 = ser.readline()
 
 temp_4 = ser.readline()
+#Encerra a conexão
 ser.close()
+
 print temp_1,temp_2,temp_3,temp_4
 
 x1 = np.array([float(temp_1),float(temp_2), float(temp_3), float(temp_4)])
 y1 = np.array([222, 385, 585, 780])
 
-#####cor do fundo do grafico
+#cor do fundo do grafico
 fig = plt.figure()
 rect = fig.patch
 rect.set_facecolor('white')
@@ -34,9 +39,6 @@ ax1.set_xlabel('tempo(milisegundos)', color='black', fontsize='20')
 ax1.set_ylabel(u'espaço(milimetros) ', color='black', fontsize='20')
 
 plt.plot(x1,y1,linestyle='-', color='green',marker='o', linewidth=3.0)
-##plt.plot(x1,y1,'ro')
-
-##plt.bar(x1,y1, width=10, color='red',align='center')
 
 plt.grid(True)
 plt.show()
