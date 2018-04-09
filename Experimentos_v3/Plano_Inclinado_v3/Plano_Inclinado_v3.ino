cd5014a5a9c1dcab89e62765570855f943f60596
YureAlbuquerque <yurealbuquerque@lme.ufc.br>
2018-04-09 17:02:51 -0300

    Versão 3 do plano inclinado iniciada

--- Experimentos_v3/Plano_Inclinado_v3/Plano_Inclinado_v3.ino
+++ Experimentos_v3/Plano_Inclinado_v3/Plano_Inclinado_v3.ino
@@ -4,8 +4,8 @@
   PROJETO: Física com Arduino
   ANALISTA: Antonio de Lisboa Coutinho Junior
   DESENVOLVEDOR: Yure Vieira Sampaio Albuquerque
-  DATA: 04/04/2018
-  VERSÃO: 2.0
+  DATA: 09/04/2018
+  VERSÃO: 3.0
   REPOSITÓRIO: GOOGLE DRIVE\UAB - Arduino\Código Arduino
 
   DESCRIÇÃO:
@@ -17,18 +17,22 @@
   Serão tomados 4 tempos.
   Calculada a velocidade do objeto.
   Transmissão via módulo Serial/Bluetooth.
-  Dados salvos em cartão micro SD.
+  Dados são mostrados em um modulo lcd 16x2 i2c.
   É necessário introduzir no Monitor Serial do Arduino a distância entre os sensores.
 
   --------------------------------------------------------------------------*/
 #include <Wire.h>
-#include <MPU6050.h>
+#include <MPU6050.h>              //Biblioteca para o modulo giroscopio/acelerômetro.
+#include <LiquidCrystal_I2C.h>    //Biblioteca para o modulo LCD-i2c.
 //Pinos para sensores(entradas analogicas)
 #define SENSOR1 4
 #define SENSOR2 5
 #define SENSOR3 6
 #define SENSOR4 7
 #define SENSOR5 8
+
+// Inicializa o display no endereco 0x3f
+LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 unsigned long t0 = 0, t1 = 0, t2 = 0, t3 = 0, t4 = 0; //Registros de tempo para cada sensor apartir de millis().
 float T1, T2 , T3, T4;                         //Tempos entre sensores.
@@ -54,6 +58,16 @@ int getAngleMPU() {
   return value;
 }
 
+void LCD_NovaMensagem(String str,int i=0,int j=0){
+  lcd.clear();
+  lcd.setCursor(i, j);
+  lcd.print(str);
+  }
+  void LCD_Mensagem(String str,int i=0,int j=0){
+  lcd.setCursor(i, j);
+  lcd.print(str);
+  }
+  
 /*****************************************************/
 void setup() {
   //Configuração de pinos como entradas digitais.
@@ -62,14 +76,17 @@ void setup() {
   pinMode (SENSOR3, INPUT);
   pinMode (SENSOR4, INPUT);
   pinMode (SENSOR5, INPUT);
+  lcd.begin (16, 2);
+  lcd.setBacklight(HIGH);
   //Comunicação serial configurada.
   Serial.begin (115200);
-  Serial.println("Inicializando MPU6050");
+  LCD_NovaMensagem("MPU6050 ...");
   //Tentativa de conexão com modulo MPU6050.
   while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
   {
-    Serial.println("Não foi possível encontrar um sensor MPU6050 válido, verificar a fiação!");
+    LCD_NovaMensagem("MPU disconectado");
     delay(500);
+    return;
   }
 }
 
@@ -78,6 +95,11 @@ void setup() {
 void loop() {
   delay(2000); // aguarda dois segundos
   Serial.println("[START]");
+  do{
+  LCD_NovaMensagem("Plano inclinado");
+  LCD_Mensagem("Angulo: "0,1);//0->primeira coluna; 1->segunda linha
+  LCD_Mensagem(String(getAngleMPU()),8,1);//8->nona coluna; 1->segunda linha
+  }while(digitalRead(SENSOR1) == LOW)
   //SENSOR1 -------------------------------------------
   while (digitalRead(SENSOR1) == HIGH);//Momento que o corpo entra no raio de cobertura do sensor
   while (digitalRead(SENSOR1) == LOW);//Momento que o corpo sai do raio de cobertura do sensor
@@ -143,3 +165,4 @@ void loop() {
   Serial.println("Reset o Arduino...");
   while (1); // loop infinito. reset o arduino para um novo experimento.
 }
+
