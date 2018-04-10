26535ab42201af513183080eeb3f8d5335b4c1fa
YureAlbuquerque <yurealbuquerque@lme.ufc.br>
2018-04-10 15:23:44 -0300

    Exemplos com lcd e bluetooth

--- Experimentos_lcd_bluetooth/Plano_Inclinado_lcd16x2/Plano_Inclinado_lcd16x2.ino
+++ Experimentos_lcd_bluetooth/Plano_Inclinado_lcd16x2/Plano_Inclinado_lcd16x2.ino
@@ -4,68 +4,116 @@
   PROJETO: Física com Arduino
   ANALISTA: Antonio de Lisboa Coutinho Junior
   DESENVOLVEDOR: Yure Vieira Sampaio Albuquerque
-  DATA: 10/04/2018
-  VERSÃO: 4.0
+  DATA: 09/04/2018
+  VERSÃO: 3.0
   REPOSITÓRIO: GOOGLE DRIVE\UAB - Arduino\Código Arduino
 
   DESCRIÇÃO:
-  Código para coleta de dados do instrumento QUEDA LIVRE.
+  Código para coleta de dados do instrumento PLANO INCLINADO.
   Possui cinco sensores de obstáculo para detectar um objeto
-  que se desloque em queda livre.
+  que se desloque em um trilho (perfil em U) e um modulo MPU6050
+  (acelerometro/giroscopio) para aferir o grau de inclinação.
   Conectorização em placa Arduino Uno R3.
   Serão tomados 4 tempos.
   Calculada a velocidade do objeto.
   Transmissão via módulo Serial/Bluetooth.
-  É necessário introduzir no Monitor Serial do Arduino a distância entre os sensores.
+  Dados são mostrados em um modulo lcd 16x2 i2c.
 
   --------------------------------------------------------------------------*/
 #include <Wire.h>
+#include <MPU6050.h>              //Biblioteca para o modulo giroscopio/acelerômetro.
 #include <LiquidCrystal_I2C.h>    //Biblioteca para o modulo LCD-i2c.
-#include <SoftwareSerial.h>
+#include <SoftwareSerial.h>       //Biblioteca usada com bluetooth
 
-#define SENSOR1 A0
-#define SENSOR2 A1
-#define SENSOR3 A2
-#define SENSOR4 A3
-#define SENSOR5 A4
+#define SENSOR1 4
+#define SENSOR2 5
+#define SENSOR3 6
+#define SENSOR4 7
+#define SENSOR5 8
 #define BOTAO 7
 #define S_TX 2
 #define S_RX 3
 
-unsigned long t0 = 0, t1 = 0, t2 = 0, t3 = 0, t4 = 0; //Registros de tempo para cada sensor apartir de millis().
-float T1, T2, T3, T4;                                 //Tempos entre sensores.
-float v1, v2, v3, v4;                                 //Velocidades médias durante a passagem por cada cada sensor.
-float distancia = 210.0;                              //Distancia entre sensores.
-
 // Inicializa o display no endereco 0x3f
 LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 SoftwareSerial bluetooth(S_RX, S_TX);
-void setup()
-{
-  lcd.begin (16, 2);
-  lcd.setBacklight(HIGH);
-  Serial.begin(115200);//Debug
-  bluetooth.begin(9600);//Porta para bluetooth
-  pinMode(SENSOR1, INPUT); //Botão
-  pinMode(SENSOR2, INPUT);
-  pinMode(SENSOR3, INPUT);
-  pinMode(SENSOR4, INPUT);
-  pinMode(SENSOR5, INPUT);
-  pinMode(BOTAO, INPUT_PULLUP);
+
+unsigned long t0 = 0, t1 = 0, t2 = 0, t3 = 0, t4 = 0; //Registros de tempo para cada sensor apartir de millis().
+float T1, T2 , T3, T4;                         //Tempos entre sensores.
+float v1, v2, v3, v4;                                 //Velocidades médias durante a passagem por cada cada sensor.
+float distancia = 210.0;
+int angulo;                                           //Angulo de inclinação.
+MPU6050 mpu;                                          //Instancia da classe usada na leitura do acelerometro/giroscopio.
+
+/*****************************************************/
+//Função que retorna o angulo de inclinação
+int getAngleMPU() {
+  //valores de correção obtido impiricamente
+  int offset = -90;
+  int factor = 1;
+  int value;
+  // Leitura dos valores normalizados
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  // Calcula Pitch & Roll
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis * normAccel.YAxis + normAccel.ZAxis * normAccel.ZAxis)) * 180.0) / M_PI;
+  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis) * 180.0) / M_PI;
+  value = factor * roll + offset;
+  return value;
 }
 
-void loop()
-{
+void LCD_NovaMensagem(String str, int i = 0, int j = 0) {
+  lcd.clear();
+  lcd.setCursor(i, j);
+  lcd.print(str);
+}
+void LCD_Mensagem(String str, int i = 0, int j = 0) {
+  lcd.setCursor(i, j);
+  lcd.print(str);
+}
+
+/*****************************************************/
+void setup() {
+  //Configuração de pinos como entradas digitais.
+  pinMode (SENSOR1, INPUT);
+  pinMode (SENSOR2, INPUT);
+  pinMode (SENSOR3, INPUT);
+  pinMode (SENSOR4, INPUT);
+  pinMode (SENSOR5, INPUT);
+  pinMode(BOTAO, INPUT_PULLUP);
+  lcd.begin (16, 2);
+  lcd.setBacklight(HIGH);
+  Serial.begin (115200);//Comunicação serial configurada.
+  bluetooth.begin(9600);//Porta para bluetooth
+  LCD_NovaMensagem("MPU6050 ...");
+  //Tentativa de conexão com modulo MPU6050.
+  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    LCD_NovaMensagem("MPU disconectado");
+    delay(500);
+    return;
+  }
+}
+
+/*****************************************************/
+// Laco principal do programa
+void loop() {
   delay(2000); // aguarda dois segundos
   Serial.println("[START]");
-  LCD_NovaMensagem("Queda Livre");
-  LCD_Mensagem("Aguardando valores!", 0, 1);
+  while (digitalRead(SENSOR5) == HIGH) { //Usando ultimo sensor como chave de escolha do angulo
+    LCD_NovaMensagem("Plano inclinado");
+    LCD_Mensagem("Angulo: ", 0, 1); //0->primeira coluna; 1->segunda linha
+    LCD_Mensagem(String(getAngleMPU()), 8, 1); //8->nona coluna; 1->segunda linha
+    delay(250);
+  }
+  LCD_NovaMensagem("Plano inclinado");
+  LCD_Mensagem("Pronto P/ uso", 0, 1);
   //SENSOR1 -------------------------------------------
-  while (digitalRead(SENSOR1) == HIGH);//Momento que o corpo entra no raio de cobertura do sensor
+  while (digitalRead(SENSOR1) == HIGH); //Momento que o corpo entra no raio de cobertura do sensor
   while (digitalRead(SENSOR1) == LOW);//Momento que o corpo sai do raio de cobertura do sensor
   t0 = millis(); // captura o tempo corrente em t0
 
-  // SENSOR2 -------------------------------------------
+  //SENSOR2 -------------------------------------------
   while (digitalRead(SENSOR2) == HIGH);//Momento que o corpo entra no raio de cobertura do sensor
   while (digitalRead(SENSOR2) == LOW);//Momento que o corpo sai do raio de cobertura do sensor
   t1 = millis(); // captura o tempo corrente em t1
@@ -84,7 +132,7 @@ void loop()
   while (digitalRead(SENSOR5) == HIGH);
   while (digitalRead(SENSOR5) == LOW);
   t4 = millis(); // captura o tempo corrente em t4
-
+  //Após a leitura de todos os sensores, calcula os tempos e velocidades.
   // Calculo -------------------------------------------
   T1 = (t1 - t0) ;
   T2 = (t2 - t0) ;
@@ -97,7 +145,9 @@ void loop()
   v3 = 3 * distancia / T3;
   v4 = 4 * distancia / T4;
 
-  //Dados enviados por bluetooth
+  //Leitura do angulo
+  angulo = getAngleMPU();
+
   bluetooth.print("Tempo1|");
   bluetooth.print(T1);
   delay(1000);
@@ -120,7 +170,8 @@ void loop()
   //         Serial.print(v4, 6);
   Serial.println("Experimento finalizado....");
   Serial.println("Reset o Arduino...");
-  while (1){ // loop infinito. reset o arduino para um novo experimento.
+  while (1) { // loop infinito. reset o arduino para um novo experimento.
+    //while (digitalRead(BOTAO) == HIGH);//Substituir um push button no futuro
     LCD_NovaMensagem("Tempo1: ");
     LCD_Mensagem(String(T1), 8, 0);
     LCD_Mensagem("Tempo2: ", 0, 1);
@@ -153,3 +204,4 @@ void loop()
     while (digitalRead(BOTAO) == HIGH);
   }
 }
+
--- Experimentos_lcd_bluetooth/Queda_Livre_lcd16x2/Queda_Livre_lcd16x2.ino
+++ Experimentos_lcd_bluetooth/Queda_Livre_lcd16x2/Queda_Livre_lcd16x2.ino
@@ -4,55 +4,49 @@
   PROJETO: Física com Arduino
   ANALISTA: Antonio de Lisboa Coutinho Junior
   DESENVOLVEDOR: Yure Vieira Sampaio Albuquerque
-  DATA:10/04/2018
-  VERSÃO: 3.0
+  DATA: 10/04/2018
+  VERSÃO: 4.0
   REPOSITÓRIO: GOOGLE DRIVE\UAB - Arduino\Código Arduino
 
   DESCRIÇÃO:
-  Código para coleta de dados do instrumento TRILHO HORIZONTAL ou QUEDA LIVRE.
+  Código para coleta de dados do instrumento QUEDA LIVRE.
   Possui cinco sensores de obstáculo para detectar um objeto
-  que se desloque em um trilho (perfil em U) ou em queda livre.
+  que se desloque em queda livre.
   Conectorização em placa Arduino Uno R3.
   Serão tomados 4 tempos.
   Calculada a velocidade do objeto.
-  Transmissão via módulo Serial.
-  Dados são mostrados em um modulo lcd 16x2 i2c.
+  Transmissão via módulo Serial/Bluetooth.
+  É necessário introduzir no Monitor Serial do Arduino a distância entre os sensores.
 
   --------------------------------------------------------------------------*/
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>    //Biblioteca para o modulo LCD-i2c.
+#include <SoftwareSerial.h>
 
-#define SENSOR1 8
-#define SENSOR2 9
-#define SENSOR3 10
-#define SENSOR4 11
-#define SENSOR5 12
+#define SENSOR1 A0
+#define SENSOR2 A1
+#define SENSOR3 A2
+#define SENSOR4 A3
+#define SENSOR5 A4
 #define BOTAO 7
+#define S_TX 2
+#define S_RX 3
+
+unsigned long t0 = 0, t1 = 0, t2 = 0, t3 = 0, t4 = 0; //Registros de tempo para cada sensor apartir de millis().
+float T1, T2, T3, T4;                                 //Tempos entre sensores.
+float v1, v2, v3, v4;                                 //Velocidades médias durante a passagem por cada cada sensor.
+float distancia = 210.0;                              //Distancia entre sensores.
 
 // Inicializa o display no endereco 0x3f
 LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-
-unsigned long t0 = 0, t1 = 0, t2 = 0, t3 = 0, t4 = 0; //Registros de tempo para cada sensor apartir de millis().
-float T1, T2 , T3, T4;                         //Tempos entre sensores.
-float v1, v2, v3, v4;                                 //Velocidades médias durante a passagem por cada cada sensor.
-float distancia = 210.0;                               //Distancia entre sensores.
-
-void LCD_NovaMensagem(String str, int i = 0, int j = 0) {
-  lcd.clear();
-  lcd.setCursor(i, j);
-  lcd.print(str);
-}
-
-void LCD_Mensagem(String str, int i = 0, int j = 0) {
-  lcd.setCursor(i, j);
-  lcd.print(str);
-}
+SoftwareSerial bluetooth(S_RX, S_TX);
 void setup()
 {
-  Serial.begin(115200);
   lcd.begin (16, 2);
   lcd.setBacklight(HIGH);
-  pinMode(SENSOR1, INPUT);
+  Serial.begin(115200);//Debug
+  bluetooth.begin(9600);//Porta para bluetooth
+  pinMode(SENSOR1, INPUT); //Botão
   pinMode(SENSOR2, INPUT);
   pinMode(SENSOR3, INPUT);
   pinMode(SENSOR4, INPUT);
@@ -64,14 +58,14 @@ void loop()
 {
   delay(2000); // aguarda dois segundos
   Serial.println("[START]");
-  LCD_NovaMensagem("Trilho Honz");
+  LCD_NovaMensagem("Queda Livre");
   LCD_Mensagem("Aguardando valores!", 0, 1);
   //SENSOR1 -------------------------------------------
   while (digitalRead(SENSOR1) == HIGH);//Momento que o corpo entra no raio de cobertura do sensor
   while (digitalRead(SENSOR1) == LOW);//Momento que o corpo sai do raio de cobertura do sensor
   t0 = millis(); // captura o tempo corrente em t0
 
-  //SENSOR2 -------------------------------------------
+  // SENSOR2 -------------------------------------------
   while (digitalRead(SENSOR2) == HIGH);//Momento que o corpo entra no raio de cobertura do sensor
   while (digitalRead(SENSOR2) == LOW);//Momento que o corpo sai do raio de cobertura do sensor
   t1 = millis(); // captura o tempo corrente em t1
@@ -103,19 +97,19 @@ void loop()
   v3 = 3 * distancia / T3;
   v4 = 4 * distancia / T4;
 
-  //  Serial.print("Tempo1|");
-  //delay(10000);
-  Serial.println(T1);
-  //  delay(1000);
-  //  Serial.print("Tempo2|");
-  Serial.println(T2);
-  //  delay(1000);
-  //  Serial.print("Tempo3|");
-  Serial.println(T3);
-  //  delay(1000);
-  //  Serial.print("Tempo4|");
-  Serial.println(T4);
-  //  delay(1000);
+  //Dados enviados por bluetooth
+  bluetooth.print("Tempo1|");
+  bluetooth.print(T1);
+  delay(1000);
+  bluetooth.print("Tempo2|");
+  bluetooth.print(T2);
+  delay(1000);
+  bluetooth.print("Tempo3|");
+  bluetooth.print(T3);
+  delay(1000);
+  bluetooth.print("Tempo4|");
+  bluetooth.print(T4);
+  delay(1000);
   //         Serial.print("v1: ");
   //         Serial.print(v1, 6);
   //         Serial.print("v2: ");
@@ -126,7 +120,7 @@ void loop()
   //         Serial.print(v4, 6);
   Serial.println("Experimento finalizado....");
   Serial.println("Reset o Arduino...");
-  while (1) { // loop infinito. reset o arduino para um novo experimento.
+  while (1){ // loop infinito. reset o arduino para um novo experimento.
     LCD_NovaMensagem("Tempo1: ");
     LCD_Mensagem(String(T1), 8, 0);
     LCD_Mensagem("Tempo2: ", 0, 1);
@@ -150,12 +144,12 @@ void loop()
     LCD_Mensagem("Vel4: ", 0, 1);
     LCD_Mensagem(String(T4), 6, 1);
     delay(2000);
-//    while (digitalRead(BOTAO) == HIGH);
-//    LCD_NovaMensagem("Angulo: ");
-//    LCD_Mensagem("????", 8, 0);
-//    LCD_Mensagem("AG: ", 0, 1);
-//    LCD_Mensagem("????", 4, 1);
-//    delay(2000);
+    while (digitalRead(BOTAO) == HIGH);
+    LCD_NovaMensagem("Angulo: ");
+    LCD_Mensagem(String(angulo), 8, 0);
+    LCD_Mensagem("AG: ", 0, 1);
+    LCD_Mensagem("????", 4, 1);
+    delay(2000);
     while (digitalRead(BOTAO) == HIGH);
   }
 }
