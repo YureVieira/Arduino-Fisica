c3162067ae8f4459d9ecf0c8b5e62ee04bf19d76
YureAlbuquerque <yurealbuquerque@lme.ufc.br>
2018-06-12 18:07:37 -0300

    Alterações

--- Codigos_testes/Cronometro_ESP_gDocs/Cronometro_ESP_gDocs.ino
+++ Codigos_testes/Cronometro_ESP_gDocs/Cronometro_ESP_gDocs.ino
@@ -2,19 +2,26 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
+#include <MPU6050.h>              //Biblioteca para o modulo giroscopio/acelerômetro.
+#include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"`
 
 #define SENSOR1 D1       //Primeiro sensor.
 #define SENSOR2 D2
-#define SENSOR3 D3
-#define SENSOR4 D5
-#define SENSOR5 D6       //Ultimo sensor.
-
-unsigned long t0; // tempo inicial
-unsigned long t1; // tempo final no sensor2
-unsigned long t2; // tempo final no sensor3
-unsigned long t3; // tempo final no sensor4
-unsigned long t4; // tempo final no sensor5
+#define SENSOR3 D5
+#define SENSOR4 D6
+#define SENSOR5 D7       //Ultimo sensor.
+#define SDA_PIN D4
+#define SCL_PIN D3
+unsigned long t0 = 0, t1 = 0, t2 = 0, t3 = 0, t4 = 0; //Registros de tempo para cada sensor apartir de millis().
+//float T1, T2 , T3, T4;                         //Tempos entre sensores.
+//float v1, v2, v3, v4;                                 //Velocidades médias durante a passagem por cada cada sensor.
+//float distancia = 210.0;
+int angulo;                                           //Angulo de inclinação.
+MPU6050 mpu;                                          //Instancia da classe usada na leitura do acelerometro/giroscopio.
+SSD1306Wire  display(0x3c, SDA_PIN, SCL_PIN);
 WiFiClientSecure client; //Cria um cliente seguro (para ter acesso ao HTTPS)
+const char* ssid = "ESP12E_AP_MODE";
+const char* password = "12345678";
 String textFix = "GET /forms/d/e/1FAIpQLSdwNWQtjetH4hj_MkRDOeBXkV6ib8LLgmO8th9dk3t6ePcz_g/formResponse?ifq";
 String tboxName = "&entry.911139776=Plano_Inclinado";
 String tbox1 = "&entry.1827900739=";
@@ -22,41 +29,54 @@ String tbox2 = "&entry.1917433581=";
 String tbox3 = "&entry.246745679=";
 String tbox4 = "&entry.716066615=";
 String tbox5 = "&entry.1880225264=";
-
-//Essa String sera uma auxiliar contendo o link utilizado pelo GET, para nao precisar ficar re-escrevendo toda hora
-///////////////////////////////////////////////////////////////////////////////////////////
+/************************************************************************************/
 void setup()
 {
   Serial.begin(115200);
   WiFiManager WFManager;
-  //WFManager.resetSettings();
-  if (!WFManager.autoConnect("ESP12E_AP_MODE", "12345678")) {
-    Serial.println( "Falha ao conectar. Excedeu o tempo limite para conexao.");
+  display.init();
+  display.flipScreenVertically();
+  display.setContrast(255);
+
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  oledNewMessage(0, 0, "[MPU]: Inicializando MPU6050");
+
+  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G, 0x68, SDA_PIN, SCL_PIN))
+  {
+    oledNewMessage(0, 0, "[MPU]: MPU6050 não encontrado!");
+    delay(500);
+  }
+  oledNewMessage(0, 0, "*WIFI: Configure o WiFi!");
+  oledAddMessage(0, 0, "\n*WIFI: Acesse a rede: " + String(ssid) + " Senha:" + String(password) + "\nIP: 192.168.4.1");
+  if (!WFManager.autoConnect(ssid, password)) {
+    //    oledNewMessage(0, display.getHeight() / 2, "[WIFI]: Falha ao conectar !");
     delay(3000);
     //Reinicia o ESP e tenta novamente ou entra em sono profundo (DeepSleep)
     ESP.reset();
-    delay(5000);
   }
-  //ESP.wdtDisable();//Desabilita o SW WDT.
-  Serial.println("Conectado!!");
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP().toString());
+  // Align text vertical/horizontal center
+  display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+  display.setFont(ArialMT_Plain_16);
+  oledNewMessage(display.getWidth() / 2, display.getHeight() / 2, "IP: " + WiFi.localIP().toString());
   pinMode(SENSOR1, INPUT_PULLUP); //Botão.
   pinMode(SENSOR2, INPUT_PULLUP);
   pinMode(SENSOR3, INPUT_PULLUP);
   pinMode(SENSOR4, INPUT_PULLUP);
   pinMode(SENSOR5, INPUT_PULLUP);
+
   delay(2000);//Espera um tempo para se conectar no WiFi
 }
 ///////////////////////////////////////////////////////////////////////////////////////////
 void loop()
 {
-  Serial.println();
-  Serial.println("Iniciando em 2s...");
+  display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+  display.setFont(ArialMT_Plain_16);
+  oledNewMessage(display.getWidth() / 2, display.getHeight() / 2, "Iniciando em 2s...");
   delay(2000); // aguarda dois segundos
-
   //SENSOR1 -------------------------------------------
   while (digitalRead(SENSOR1) == HIGH) {
+    oledNewMessage(display.getWidth() / 2, display.getHeight() / 2, "Inclinação: " + String(getAngleMPU()) + "°");
     yield();//Efetua o feed do SW WDT
     ESP.wdtFeed();
   }
@@ -65,7 +85,9 @@ void loop()
     ESP.wdtFeed();
   }
   t0 = millis(); // captura o tempo corrente em t0
-  Serial.println("t0");
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  oledNewMessage(0, 0, "t0 = " + String(t0));
 
   // SENSOR2 -------------------------------------------
   while (digitalRead(SENSOR2) == HIGH) {
@@ -77,7 +99,7 @@ void loop()
     ESP.wdtFeed();
   }
   t1 = millis(); // captura o tempo corrente em t1
-  Serial.println("t1");
+  oledAddMessage(0, 11, "t1 = " + String(t1));
 
   //SENSOR3---------------------------------------------
   while (digitalRead(SENSOR3) == HIGH) {
@@ -89,7 +111,7 @@ void loop()
     ESP.wdtFeed();
   }
   t2 = millis(); // captura o tempo corrente em t2
-  Serial.println("t2");
+  oledAddMessage(0, 22, "t2 = " + String(t2));
 
   //SENSOR4---------------------------------------------
   while (digitalRead(SENSOR4) == HIGH) {
@@ -101,7 +123,7 @@ void loop()
     ESP.wdtFeed();
   }
   t3 = millis(); // captura o tempo corrente em t3
-  Serial.println("t3");
+  oledAddMessage(0, 33, "t3 = " + String(t3));
 
   //SENSOR5---------------------------------------------
   while (digitalRead(SENSOR5) == HIGH) {
@@ -113,18 +135,19 @@ void loop()
     ESP.wdtFeed();
   }
   t4 = millis(); // captura o tempo corrente em t4
-  Serial.println("t4");
+  oledAddMessage(0, 44, "t4 = " + String(t4));
 
   // Calculo -------------------------------------------
-  Serial.print("Tempo decorrido 2 (ms): ");
-  Serial.println(t1 - t0);
-  Serial.print("Tempo decorrido 3 (ms): ");
-  Serial.println(t2 - t0);
-  Serial.print("Tempo decorrido 4 (ms): ");
-  Serial.println(t3 - t0);
-  Serial.print("Tempo decorrido 5 (ms): ");
-  Serial.println(t4 - t0);
-
+  //  Serial.print("Tempo decorrido 2 (ms): ");
+  //  Serial.println(t1 - t0);
+  //  Serial.print("Tempo decorrido 3 (ms): ");
+  //  Serial.println(t2 - t0);
+  //  Serial.print("Tempo decorrido 4 (ms): ");
+  //  Serial.println(t3 - t0);
+  //  Serial.print("Tempo decorrido 5 (ms): ");
+  //  Serial.println(t4 - t0);
+  display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+  display.setFont(ArialMT_Plain_16);
   if (client.connect("docs.google.com", 443) == 1)//Tenta se conectar ao servidor do Google docs na porta 443 (HTTPS)
   {
     Serial.println("Conectado ao servidor docs.google.com");
@@ -138,24 +161,60 @@ void loop()
     toSend += String(t3 - t0);
     toSend += tbox4;// pergunta 4
     toSend += String(t4 - t0);
-    toSend += tbox5+"Nah";
+    toSend += tbox5 + String(getAngleMPU());
     toSend += "&submit=Submit HTTP/1.1";//Completamos o metodo GET para nosso formulario.
-    Serial.println(toSend);
+    //    Serial.println(toSend);
     client.println(toSend);//Enviamos o GET ao servidor-
     client.println("Host: docs.google.com");//-
     client.println();//-
     client.stop();//Encerramos a conexao com o servidor
-    Serial.println("Dados enviados.");//Mostra no monitor que foi enviado
+    oledNewMessage(display.getWidth() / 2, display.getHeight() / 2, "Dados enviados."); //Mostra no monitor que foi enviado
   }
   else
   {
-    Serial.println("Erro ao se conectar");//Se nao for possivel conectar no servidor, ira avisar no monitor.
+    oledNewMessage(display.getWidth() / 2, display.getHeight() / 2, "Erro ao se conectar"); //Se nao for possivel conectar no servidor, ira avisar no monitor.
   }
-
-  Serial.println("Experimento finalizado....");
-  Serial.println("Reset o ESP8266...");
+  //  Serial.println("Experimento finalizado....");
+  //  Serial.println("Reset o ESP8266...");
+  delay(3000);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  oledNewMessage(0, 0, "Tempo 1 (ms): " + String(t1 - t0));
+  oledAddMessage(0, 0, "\nTempo 2 (ms): " + String(t2 - t0));
+  oledAddMessage(0, 0, "\n\nTempo 3 (ms): " + String(t3 - t0));
+  oledAddMessage(0, 0, "\n\n\nTempo 4 (ms): " + String(t4 - t0));
+  oledAddMessage(0, 0, "\n\n\n\nExperimento finalizado....");
   long counter = millis();
-  while (millis() - counter < 5000) {
-    ESP.wdtFeed(); // loop de 5 segundos.
+  //  while (millis() - counter < 60000) {
+  //    ESP.wdtFeed();
+  //  }
+  while (1) {
+    ESP.wdtFeed();
   }
+}
+/****************** FUNÇÕES P/ OLED ***********************************/
+void oledNewMessage(int16_t x, int16_t y, String msg) {
+  display.clear();
+  display.drawStringMaxWidth(x, y, display.getWidth(), msg);
+  display.display();
+}
+void oledAddMessage(int16_t x, int16_t y, String msg) {
+  display.drawStringMaxWidth(x, y, display.getWidth(), msg);
+  display.display();
+}
+/****************** FUNÇÕES MPU6050 ***********************************/
+//Função que retorna o angulo de inclinação
+int getAngleMPU() {
+  //valores de correção obtido impiricamente
+  int offset = -91;
+  int factor = 1;
+  int value;
+  // Leitura dos valores normalizados
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  // Calcula Pitch & Roll
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis * normAccel.YAxis + normAccel.ZAxis * normAccel.ZAxis)) * 180.0) / M_PI;
+  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis) * 180.0) / M_PI;
+  value = factor * roll + offset;
+  return value;
 }
--- Codigos_testes/ESP_P_Inclinado/ESP_P_Inclinado.ino
+++ Codigos_testes/ESP_P_Inclinado/ESP_P_Inclinado.ino
@@ -2,19 +2,17 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
-
-
-#include <ESP8266mDNS.h>
-#include <ArduinoOTA.h>
-
 #include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
 #include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"`
+#include <MPU6050.h>
 /*************************************************************************************/
 #define SENSOR1 D3       //Primeiro sensor.
 #define SENSOR2 D4
 #define SENSOR3 D5
 #define SENSOR4 D6
 #define SENSOR5 D7       //Ultimo sensor.
+#define SDA_PIN D2
+#define SCL_PIN D1
 
 unsigned long t0; // tempo inicial
 unsigned long t1; // tempo final no sensor2
@@ -22,38 +20,46 @@ unsigned long t2; // tempo final no sensor3
 unsigned long t3; // tempo final no sensor4
 unsigned long t4; // tempo final no sensor5
 /***********************************************************************************/
+const char* ssid = "ESP12E_AP_MODE";
+const char* password = "12345678";
+const char* INDEX_HTML = "<!DOCTYPE html> <html> <head> <!--<meta http-equiv=\"refresh\" content=\"2\">--> <title>Plano Inclinado</title> <style type=\"text/css\"> body{ font-family: Helvetica, sans-serif; font-weight: bold; } .button { border: none; border-radius: 8px; padding: 11px 22px; text-align: center; font-size: 16px; margin: 4px 2px; /*transition-duration: 0.4s;*/ cursor: pointer; } input[type=submit] { background-color: #f6863c; background-image: -webkit-gradient(linear, left top, left bottom, from(#f8a56d), to(#f6863c)); color: white; } input[type=submit]:hover { /*border:1px solid #ff9913;*/ background-color: #ddd; background-image: -webkit-gradient(linear, left top, left bottom, from(#fff), to(#bbb)); color: black; } </style> </head> <body> <h1>Plano Inclinado</h1> <div class=\"topMenu-div\"> <a href=\"\">Dimensões</a> <a href=\"\">Rede</a> <a href=\"\">Log</a> <a href=\"\">Sobre</a> </div> <div class=\"form-div\"> <form class=\"form-style\" action=\"/\" method=\"get\"> <p><label>1º Tempo:</label> <input type=\"text\" name=\"input1\"></p> <p><label>2º Tempo:</label> <input type=\"text\" name=\"input2\"></p> <p><label>3º Tempo:</label> <input type=\"text\" name=\"input3\"></p> <p><label>4º Tempo:</label> <input type=\"text\" name=\"input4\"></p> <hr> <div id=\"time-input\"> <p> <label>Unidade de tempo:</label> <select name=\"time\"> <option value=\"1\">Segundos(s)</option> <option value=\"1000\">Milisegundos(ms)</option> <option value=\"1000000\">Microsegundos(us)</option> </select> </p> </div> <div id=\"space-input\"> <p> <label>Unidade de espaço:</label> <select name=\"space\"> <option value=\"1\">Metros(m)</option> <option value=\"1000\">Milimetros(mm)</option> </select> </p> </div> <div class=\"submit-div\"> <input class=\"button\" type=\"submit\" value=\"Ok\"> </div> </form> </div> </body> </html>";
 
-int sda = D2, scl = D1;
-SSD1306Wire  display(0x3c, sda, scl);
-// SH1106 display(0x3c, D3, D5);
-
+ESP8266WebServer server(80);//webserver
+SSD1306Wire  display(0x3c, SDA, SCL);
+int angulo;                                           //Angulo de inclinação.
+MPU6050 mpu;
 
 void setup() {
-  WiFiManager WFManager;
   Serial.begin(115200);
-
-  if (!WFManager.autoConnect("ESP12E_AP_MODE", "12345678")) {
-    display.clear();
-    display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(0, display.getHeight() / 2, "Falha ao conectar!");
-    display.display();
-    delay(3000);
-    //Reinicia o ESP e tenta novamente ou entra em sono profundo (DeepSleep)
-    ESP.reset();
-  }
-
+  WiFiManager WFManager;
+  //  WFManager.resetSettings();
+  //Inicializa display oled
   display.init();
   display.flipScreenVertically();
   display.setContrast(255);
 
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  oledNewMessage(0, 0, "[MPU]: Inicializando MPU6050");
+
+  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G, 0x68, SDA_PIN, SCL_PIN))
+  {
+    oledNewMessage(0, 0, "[MPU]: MPU6050 não encontrado!");
+    delay(500);
+  }
+  oledNewMessage(0, 0, "*WIFI: Configure o WiFi!");
+  oledAddMessage(0, 0, "\n*WIFI: Acesse a rede: " + String(ssid) + " Senha:" + String(password) + "\nIP: 192.168.4.1");
+  if (!WFManager.autoConnect(ssid, password)) {
+    //    oledNewMessage(0, display.getHeight() / 2, "[WIFI]: Falha ao conectar !");
+    delay(3000);
+    //Reinicia o ESP e tenta novamente ou entra em sono profundo (DeepSleep)
+    ESP.reset();
+  }
   // Align text vertical/horizontal center
   display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-  display.setFont(ArialMT_Plain_10);
-  display.drawString(display.getWidth() / 2, 10, "IP: " + WiFi.localIP().toString());
-  //  display.drawString(display.getWidth() / 2, display.getHeight() / 2, "Falha ao conectar!");
+  display.setFont(ArialMT_Plain_16);
+  oledNewMessage(display.getWidth() / 2, display.getHeight() / 2, "IP: " + WiFi.localIP().toString());
   //display.drawXbm(4,32, WiFi_Logo_width, WiFi_Logo_height, WiFi_Logo_bits);
-  display.display();
 
   pinMode(SENSOR1, INPUT_PULLUP); //Botão.
   pinMode(SENSOR2, INPUT_PULLUP);
@@ -61,27 +67,30 @@ void setup() {
   pinMode(SENSOR4, INPUT_PULLUP);
   pinMode(SENSOR5, INPUT_PULLUP);
   delay(5000);
+
   //Abre o sistema de arquivos (mount)
   openFS();
   //Cria o arquivo caso o mesmo não exista
   createFile("log.txt");
-  writeFile("log.txt", "Booting ESP8266...");
-  writeFile("log.txt", "Connecting to " + WiFi.SSID());
+  writeFile("log.txt", "Arrancando ESP8266...");
+  writeFile("log.txt", "Conectado a rede " + WiFi.SSID());
+
+  server.on("/", handleRoot);
+  server.onNotFound(handleNotFound);
+  server.begin();
 }
 
 void loop() {
-  display.clear();
   display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
   display.setFont(ArialMT_Plain_16);
-  display.drawString(display.getWidth() / 2, display.getHeight() / 2, "Iniciando em 2s...");
-  display.display();
+  oledNewMessage(display.getWidth() / 2, display.getHeight() / 2, "Iniciando em 2s...");
   delay(2000); // aguarda dois segundos
+  //  oledNewMessage(display.getWidth() / 2, display.getHeight() / 2, "Sistema pronto!");
 
-  display.clear();
-  display.drawString(display.getWidth() / 2, display.getHeight() / 2, "Sistema pronto!");
-  display.display();
   //SENSOR1 -------------------------------------------
   while (digitalRead(SENSOR1) == HIGH) {
+    server.handleClient();
+    oledNewMessage(display.getWidth() / 2, display.getHeight() / 2, "Inclinação: " + String(getAngleMPU()) + "°");
     yield();//Efetua o feed do SW WDT
     ESP.wdtFeed();
   }
@@ -90,11 +99,9 @@ void loop() {
     ESP.wdtFeed();
   }
   t0 = millis(); // captura o tempo corrente em t0
-  display.clear();
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_10);
-  display.drawString(0, 0, "t0 = " + String(t0));
-  display.display();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  oledNewMessage(0, 0, "t0 = " + String(t0));
 
   // SENSOR2 -------------------------------------------
   while (digitalRead(SENSOR2) == HIGH) {
@@ -106,8 +113,7 @@ void loop() {
     ESP.wdtFeed();
   }
   t1 = millis(); // captura o tempo corrente em t1
-  display.drawString(0, 10, "\nt1 = " + String(t1));
-  display.display();
+  oledAddMessage(0, 11, "t1 = " + String(t1));
 
   //SENSOR3---------------------------------------------
   while (digitalRead(SENSOR3) == HIGH) {
@@ -119,8 +125,7 @@ void loop() {
     ESP.wdtFeed();
   }
   t2 = millis(); // captura o tempo corrente em t2
-  display.drawString(0, 20, "t2 = " + String(t2));
-  display.display();
+  oledAddMessage(0, 22, "t2 = " + String(t2));
 
   //SENSOR4---------------------------------------------
   while (digitalRead(SENSOR4) == HIGH) {
@@ -132,8 +137,7 @@ void loop() {
     ESP.wdtFeed();
   }
   t3 = millis(); // captura o tempo corrente em t3
-  display.drawString(0, 30, "t3 = " + String(t3));
-  display.display();
+  oledAddMessage(0, 33, "t3 = " + String(t3));
 
   //SENSOR5---------------------------------------------
   while (digitalRead(SENSOR5) == HIGH) {
@@ -145,32 +149,41 @@ void loop() {
     ESP.wdtFeed();
   }
   t4 = millis(); // captura o tempo corrente em t4
-  display.drawString(0, 40, "t4 = " + String(t4));
-  display.display();
-
+  oledAddMessage(0, 44, "t4 = " + String(t4));
+  server.handleClient();
   // Calculo -------------------------------------------
   delay(2000);
-  display.clear();
-  display.drawString(0, 0, "Tempo 1 (ms): " + String(t1 - t0));
-  display.drawString(0, 10, "Tempo 2 (ms): " + String(t2 - t0));
-  display.drawString(0, 20, "Tempo 3 (ms): " + String(t3 - t0));
-  display.drawString(0, 30, "Tempo 4 (ms): " + String(t4 - t0));
-  display.drawString(0, 40, "Experimento finalizado....");
-  display.display();
+
+  oledNewMessage(0, 0, "Tempo 1 (ms): " + String(t1 - t0));
+  oledAddMessage(0, 0, "\nTempo 2 (ms): " + String(t2 - t0));
+  oledAddMessage(0, 0, "\n\nTempo 3 (ms): " + String(t3 - t0));
+  oledAddMessage(0, 0, "\n\n\nTempo 4 (ms): " + String(t4 - t0));
+  oledAddMessage(0, 0, "\n\n\n\nExperimento finalizado....");
+
   String log = "****RESULTADOS****";
   log += "\nTempo 1 (ms): " + String(t1 - t0);
   log += "\nTempo 2 (ms): " + String(t2 - t0);
   log += "\nTempo 3 (ms): " + String(t3 - t0);
   log += "\nTempo 4 (ms): " + String(t4 - t0);
   log += "\nExperimento finalizado";
-  writeFile("log.txt",log);
+  //Escreva no display
+  //  oledNewMessage(0,0,log);
+  //Escreva no arquivo
+  writeFile("log.txt", log);
+  //Escreva no HTML
+//  String indexMod = 
+  //Laço de 50s
   long counter = millis();
   while (millis() - counter < 50000) {
+    server.handleClient();
     ESP.wdtFeed(); // loop de 5 segundos.
   }
   Serial.println(readFile("log.txt"));
 }
-/*****************************************************************************/
+/****************** FUNÇÕES P/ LEITURA/ESCRITA DE ARQUIVOS**************/
+void formatFS() {
+  SPIFFS.format();
+}
 void createFile(String file) {
   File wFile;
 
@@ -244,4 +257,54 @@ void openFS(void) {
   } else {
     Serial.println("Sistema de arquivos aberto com sucesso!");
   }
+}
+/****************** FUNÇÕES P/ OLED ***********************************/
+void oledNewMessage(int16_t x, int16_t y, String msg) {
+  display.clear();
+  display.drawStringMaxWidth(x, y, display.getWidth(), msg);
+  display.display();
+}
+void oledAddMessage(int16_t x, int16_t y, String msg) {
+  display.drawStringMaxWidth(x, y, display.getWidth(), msg);
+  display.display();
+}
+/****************** FUNÇÕES MPU6050 ***********************************/
+//Função que retorna o angulo de inclinação
+int getAngleMPU() {
+  //valores de correção obtido impiricamente
+  int offset = -91;
+  int factor = 1;
+  int value;
+  // Leitura dos valores normalizados
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  // Calcula Pitch & Roll
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis * normAccel.YAxis + normAccel.ZAxis * normAccel.ZAxis)) * 180.0) / M_PI;
+  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis) * 180.0) / M_PI;
+  value = factor * roll + offset;
+  return value;
+}
+/**************** FUNÇÕES DO WEBSERVER *****************************/
+void handleRoot() {
+  server.send(200, "text/html", INDEX_HTML);
+}
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+/************************* DIVERSOS **************************/
+void combo() {
+  server.handleClient();
+  yield();
+  ESP.wdtFeed();
 }
